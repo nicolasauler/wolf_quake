bbef5f4bc3f6beb4a8d52cf38b29fa15e8eb4a85
Nicolas Auler <nicolasauler@usp.br>
2024-03-28 23:40:37 -0300

    Fixing a test that wasn't covering what it was supposed to. Thanks codecov.

--- src/quake3_parser/parser.rs
+++ src/quake3_parser/parser.rs
@@ -634,7 +634,6 @@ mod tests {
         0:04 ClientUserinfoChanged: 3 n\Mocinha\t\0\model\sarge\hmodel\sarge\g_redteam\\g_blueteam\\c1\4\c2\5\hc\95\w\0\l\0\tt\0\tl\0
         0:05 Kill: 2 3 7: Dono da bola killed Mocinha by MOD_ROCKET_SPLASH
         0:06 Kill: 3 2 7: Mocinha killed Dono da bola by MOD_ROCKET_SPLASH
-        0:07 ShutdownGame:
         26 0:07 ------------------------------------------------------------
         0:08 InitGame: \sv_floodProtect\1\sv_maxPing\0\sv_minPing\0\sv_maxRate\10000\sv_minRate\0\sv_hostname\Code Miner Server\g_gametype\0\sv_privateClients\2\sv_maxclients\16\sv_allowDownload\0\bot_minplayers\0\dmflags\0\fraglimit\20\timelimit\15\g_maxGameClients\0\capturelimit\8\version\ioq3 1.36 linux-x86_64 Apr 12 2009\protocol\68\mapname\q3dm17\gamename\baseq3\g_needpass\0
         0:09 ClientConnect: 2
